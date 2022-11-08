09858b027dadd6fe5d692a1044594c5a0bf5de04
nadim-sheikh <rifatdesign07@gmail.com>
2022-11-08 11:58:18 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}","node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    ],
+  },
+  plugins: [require('flowbite/plugin')],
+}
